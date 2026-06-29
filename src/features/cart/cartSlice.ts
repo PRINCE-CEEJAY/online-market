386fd60260e9a33210379f49882731cd5f7be014
PRINCE-CEEJAY <ceejaydroidprince@gmail.com>
2026-06-29 07:33:23 -0400

    fixed cartSlice bug

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -8,22 +8,7 @@ interface CartType {
 }
 
 const initialCart: CartType = {
-  cart: [
-    // {
-    //   id: 1,
-    //   title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-    //   price: 109.95,
-    //   description:
-    //     'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-    //   category: "men's clothing",
-    //   image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
-    //   rating: {
-    //     rate: 3.9,
-    //     count: 120,
-    //   },
-    //   quantity: 1,
-    // },
-  ],
+  cart: [],
 };
 export const cartSlice = createSlice({
   name: 'cart',
@@ -82,4 +67,4 @@ export const {
   removeFromCart,
 } = cartSlice.actions;
 
-export default cartSlice;
+export default cartSlice.reducer;
