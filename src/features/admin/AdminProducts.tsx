5706c85acff1808e103b2a8bc3dfab27edfa6d0a
PRINCE CEEJAY <ceejaydroidprince@gmail.com>
2026-07-02 15:40:21 +0100

    refactoring

--- src/features/admin/AdminProducts.tsx
+++ src/features/admin/AdminProducts.tsx
@@ -3,15 +3,15 @@ import { useGetProductsQuery } from '@/services/firebaseApi';
 export default function AdminProducts() {
   const { data: products, isLoading, isError } = useGetProductsQuery();
 
-  if (isLoading) return console.log('loading from firebase...');
+  if (isLoading) return <h1>'loading from firebase...'</h1>;
 
-  if (isError) return null;
+  if (isError) return <h1>Error</h1>;
 
-  console.log(products);
-
-  return (
-    <div>
-      <h1>Products</h1>
-    </div>
-  );
+  if (products) {
+    return (
+      <div>
+        <h1>Products</h1>
+      </div>
+    );
+  }
 }
